aab5945db6d3bae0cfb34ee62cd46db90c50336f
mwalker8030 <ban8029@protonmail.com>
2023-02-07 16:01:47 -0800

    finished task 1 of 5 for project ASCII_Mirror

--- ASCII Mirror/task/src/asciimirror/Main.java
+++ ASCII Mirror/task/src/asciimirror/Main.java
@@ -2,6 +2,15 @@ package asciimirror;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("""
+                                    _______\s
+                                   < hello >
+                                    -------\s
+                            ^__^   /       \s
+                    _______/(oo)  /        \s
+                /\\/(       /(__)           \s
+                   | w----||               \s
+                   ||     ||               \s
+                   """);
     }
 }
